bb66a647905f80c419c30cafd8d87ce1d76423e8
Justin Scott <jjustin@gmail.com>
2013-10-22 06:20:48 +0100

    new layout and conversion to php

--- index.php
+++ index.php
@@ -1,1 +1,130 @@
-Ed Friel Copper Run will be coming soon.
+<?php
+require "header.php";
+?>
+
+<div id="wrapper">
+	<div class="5grid-layout" id="welcome">
+		<div class="row">
+			<div class="12u">
+				<section class="content">
+					<h2>Integer gravida nibh quis urna</h2>
+					<p><a href="#"><img src="images/pics06.jpg" alt=""></a>Welcome to <strong>Azure</strong>, a free responsive HTML5 website template designed by <a href="http://html5templates.com/">HTML5Templates.com</a>. It features a modern business-oriented design, 
+						content-dense layout, and a neutral color scheme.  And like all of our templates, it is built on HTML5 and 
+						CSS3, has full responsive support for desktops, tablets, and mobile devices, and is 100% free to use 
+						personally or commercially under the <a href="http://html5templates.com/license/">Creative Commons license</a>. 
+						Be sure to check out our <a href="http://html5templates.com/">website</a> for more awesome free 
+						HTML5 templates and follow us on <a href="http://twitter.com/HTML5T">Twitter</a> for updates and new releases! Enjoy!</p>
+					<p>&nbsp;</p>
+					<p>Quisque dictum. Integer nisl risus, sagittis convallis, rutrum id, elementum congue, nibh. Suspendisse dictum porta lectus. Donec placerat odio vel elit. Nullam ante orci, pellentesque eget, tempus quis, ultrices in, est. Curabitur sit amet nulla. Nam in massa. Sed vel tellus.</p>
+					<p><a href="#" class="button-style1">Read Full Article</a></p>
+				</section>
+			</div>
+		</div>
+	</div>
+	<div class="5grid-layout" id="page-wrapper">
+		<div class="row">
+			<div class="3u">
+				<section id="pbox1">
+					<h2>Latest News</h2>
+					<ul class="style4">
+						<li class="first">
+							<p class="date"><a href="#">Novermber 01, 2012</a></p>
+							<p><a href="#">Pellentesque viverra vulputate enim. Aliquam erat volutpat. Pellentesque tristique ante ut risus. </a></p>
+						</li>
+						<li>
+							<p class="date"><a href="#">October 31, 2012</a></p>
+							<p><a href="#">Quisque dictum. Integer nisl risus, sagittis  elementum congue, nibh. Suspendisse dictum porta lectus.</a></p>
+						</li>
+						<li>
+							<p class="date"><a href="#">October21, 2012</a></p>
+							<p><a href="#">Sed vel tellus. Curabitur sem urna, consequat vel, suscipit in, mattis placerat, nulla. Sed ac leo. </a></p>
+						</li>
+					</ul>
+				</section>
+			</div>
+			<div class="6u">
+				<section id="pbox2">
+					<h2>tellus feugiat eleifend</h2>
+					<p><a href="#"><img src="images/pics07.jpg" alt=""></a></p>
+					<p>Pellentesque viverra vulputate enim. Aliquam erat volutpat. Pellentesque tristique ante ut risus. Quisque dictum. Integer nisl risus, sagittis convallis, rutrum id, elementum congue, nibh. Suspendisse dictum porta lectus. Donec placerat odio vel elit. Nullam ante orci, pellentesque eget, tempus quis, ultrices in, est. Curabitur sit amet nulla. Nam in massa. Sed vel tellus. </p>
+					<p><a href="#" class="button-style1">Read Full Article</a></p>
+				</section>
+			</div>
+			<div class="3u">
+				<section id="pbox3">
+					<h2>Pellentesque</h2>
+					<ul class="style5">
+						<li class="first"><a href="#">Maecenas luctus lectus  sapien</a></li>
+						<li><a href="#">Donec dictum metus in sapien</a></li>
+						<li><a href="#">Integer gravida nibh quis urna</a></li>
+						<li><a href="#">Etiam posuere augue sit amet </a></li>
+						<li><a href="#">Mauris vulputate dolor sit amet </a></li>
+						<li><a href="#">Praesent scelerisque scelerisque </a></li>
+						<li><a href="#">Integer gravida nibh quis urna</a></li>
+						<li><a href="#">Etiam posuere augue  amet nisl</a></li>
+						<li><a href="#">Mauris vulputate dolor sit  nibh</a></li>
+					</ul>
+					<p><a href="#" class="button-style1">Read Full Article</a></p>
+				</section>
+			</div>
+		</div>
+	</div>
+	<div class="5grid-layout" id="footer">
+		<div class="row">
+			<div class="8u">
+				<section id="fbox1">
+					<h2>Maecenas luctus lectus</h2>
+					<p>Pellentesque viverra vulputate enim. Aliquam erat volutpat. Pellentesque tristique ante ut risus. Quisque dictum. Integer nisl risus, sagittis convallis, rutrum id, elementum congue, nibh. Suspendisse dictum porta lectus. Donec placerat odio vel elit. Nullam ante orci, pellentesque eget, tempus quis, ultrices in, est. Curabitur sit amet nulla. Nam in massa. Sed vel tellus.</p>
+					<ul class="style1">
+						<li>
+							<p><a href="#"><img src="images/pics01.jpg" alt=""></a></p>
+							<p>Pellentesque viverra aliquam.</p>
+							<p><a href="#" class="button-style1">Read More</a></p>
+						</li>
+						<li>
+							<p><a href="#"><img src="images/pics02.jpg" alt=""></a></p>
+							<p>Pellentesque viverra aliquam.</p>
+							<p><a href="#" class="button-style1">Read More</a></p>
+						</li>
+						<li>
+							<p><a href="#"><img src="images/pics03.jpg" alt=""></a></p>
+							<p>Pellentesque viverra aliquam.</p>
+							<p><a href="#" class="button-style1">Read More</a></p>
+						</li>
+						<li>
+							<p><a href="#"><img src="images/pics04.jpg" alt=""></a></p>
+							<p>Pellentesque viverra aliquam.</p>
+							<p><a href="#" class="button-style1">Read More</a></p>
+						</li>
+						<li>
+							<p><a href="#"><img src="images/pics05.jpg" alt=""></a></p>
+							<p>Pellentesque viverra aliquam.</p>
+							<p><a href="#" class="button-style1">Read More</a></p>
+						</li>
+					</ul>
+				</section>
+			</div>
+			<div class="4u">
+				<section id="fbox2">
+					<h2>Nulla  eleifend</h2>
+					<ul class="style2">
+						<li>
+							<p><span class="date">20012.10.25</span><span class="heading-title">Etiam posuere augue</span></p>
+							<p><a href="#">Integer nisl risus, sagittis convallis, rutrum id, elementum congue, nibh. Suspendisse dictum porta lectus. </a></p>
+						</li>
+						<li>
+							<p><span class="date">20012.10.25</span><span class="heading-title">Etiam posuere augue</span></p>
+							<p><a href="#">Quisque dictum. Integer nisl risus, sagittis elementum congue, nibh. Suspendisse dictum porta lectus. </a></p>
+						</li>
+						<li>
+							<p><span class="date">20012.10.25</span><span class="heading-title">Etiam posuere augue</span></p>
+							<p><a href="#">Quisque dictum. Integer nisl risus, sagittis elementum congue, nibh. Suspendisse dictum porta lectus. </a></p>
+						</li>
+					</ul>
+				</section>
+			</div>
+		</div>
+	</div>
+</div>
+<?php
+require "footer.php";
