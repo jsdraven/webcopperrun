cecc8d0a6cb98ebe9b269c9e46e194bb1bdf98c0
Justin Scott <jjustin@gmail.com>
2013-12-11 00:16:18 -0800

    minor up keeping

--- index.php
+++ index.php
@@ -25,8 +25,55 @@ require "header.php";
 			</div>
 			<div class="6u mobileUI-main-content">
 				<section id="pbox2">
-					<h2>Just a heads up</h2>
-					<p>Here is where you would like to put leading information and possibly even a shoutout to a shinning star</p>
+					<div align="center">
+                    <br />
+					  <p>Please join us Saturday January 25th, 2014 for the <br /> <H2>ED FRIEL MEMORIAL COPPER RUN</H2></p>
+                      <p>Saturday, January 25, 2014, RAIN OR SHINE</p>
+                      <table border="0">
+                      	<tr>
+                        	<td> TIME: 
+                            </td>
+                            <td>
+                            2 Mile Run
+                            </td>9:00 AM
+                            </td>
+                            </tr>
+					  <p>1/2 Mile Run 9:45 AM</p>
+					  <p> 10K Run 10:15 AM</p>
+					  <p> T-Shirts for all race participants – 1st
+					  
+					  , 2nd
+					  
+					  , and 3rd
+					  
+					  place Medals in all age divisions.					  </p>
+					  <p>The Copper Run 2014 lives on…because of You</p>
+					  <p align="left"> Twenty two years have passed since the Ed Friel Memorial Copper Run was founded. Since 
+					  
+					  1992, the generous support and participation of the Copper Run has accomplished so 
+					  
+					  much. We have been able to help preserve added safety for the entire Copperopolis Community 
+					  
+					  from the Volunteer Fire Department; and provide educational and sports programs to elementary 
+					  
+					  school children that would not be possible without donations from local businesses.					  </p>
+					  <p align="left">&nbsp;</p>
+					  <p align="left">These funds will be used to help cover the costs of our educational programs and on-going sports 
+					  
+					  projects at the school and provide continued support for the Volunteer Firefighter’s Association, as 
+					  
+					  well as provide assistance to the Copperopolis library. As much as we’ve accomplished, there is 
+					  
+					  still more work to be done.					  </p>
+					  <p align="left">&nbsp;</p>
+					  <p align="left">If you can contribute as a runner or a sponsor, we thank you for your support!
+					  
+					  Your donation is a gift for the future ... for our children and grandchildren…and a more secure 
+					  
+					  future for all the citizens of Copperopolis.					  </p>
+					  <p align="left">&nbsp;</p>
+					  <p align="left">Come for the race and stay for the weekend and enjoy the Sierra Foothills. </p>
+					</div>
 				</section>
 			</div>
 			<div class="3u">
