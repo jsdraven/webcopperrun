72e286fd0e1affb2e406422983b1f1534483be33
unknown <ittemp@FD0LT20.FosterDairyFarms.com>
2013-12-24 10:04:39 -0800

    fixed the kwick serv info

--- index.php
+++ index.php
@@ -13,12 +13,12 @@ require "header.php";
 							<p><a href="http://www.blackoakcasino.com"  title="A big thanks to Black Oak Casino Resort" target=NEW><img src="images/bocBnG.jpg"></a></p>
 						</li>
 						<li>
-							<p class="date"><a href="http://paylesscopperopolis.iga.com/">Young's Payless IGA</a></p>
+							<p class="date"><a href="http://paylesscopperopolis.iga.com/" title="A Big thanks to Young's Payless IGA" target=NEW>Young's Payless IGA</a></p>
 							<p><a href="http://paylesscopperopolis.iga.com/" title="A big thanks to Young's Payless IGA" target=NEW><img src="images/IGA.jpg" /></a></p>
 						</li>
 						<li>
-							<p class="date"><a href="#">Kwik Serv of Copperopolis</a></p>
-							<p><a href="#"></a></p>
+							<p class="date"><a href="http://www.boyett.net" title="Thanks to Kwick Serv in Copperopolis" target=NEW>Kwik Serv of Copperopolis</a></p>
+							<p><a href="http://www.boyett.net" title="Thanks to Kwick Serv in Copperopolis" target=NEW><img src="images/KwikServ.jpg"</a></p>
 						</li>
 					</ul>
 				</section>
